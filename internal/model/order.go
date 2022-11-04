3f1706c19cce369fc7f500b75c303cce6f0952b6
arvinpaundra <arvinpaundra06@gmail.com>
2022-11-04 22:28:25 +0700

    Fix: Update move TotalPayment from Payment to Order

--- internal/model/order.go
+++ internal/model/order.go
@@ -6,6 +6,7 @@ type Order struct {
 	ID           string        `json:"id" gorm:"primaryKey;size:255"`
 	UserId       string        `json:"user_id" gorm:"size:255"`
 	PaymentId    string        `json:"payment_id" gorm:"size:255"`
+	TotalPayment float32       `json:"total_payment"`
 	TotalQty     int           `json:"total_qty"`
 	TotalHour    int           `json:"total_hour"`
 	OrderDetails []OrderDetail `json:"order_details,omitempty"`
--- internal/model/payment.go
+++ internal/model/payment.go
@@ -4,7 +4,6 @@ import "time"
 
 type Payment struct {
 	ID            string    `json:"id" gorm:"size:255"`
-	TotalPayment  float32   `json:"total_payment"`
 	PaymentStatus string    `json:"payment_status" gorm:"size:20"`
 	PaymentType   string    `json:"payment_type" gorm:"size:50"`
 	CreatedAt     time.Time `json:"created_at"`
