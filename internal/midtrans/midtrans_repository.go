7c5e260f051a2cbe7085bb4640e5d5e89402d7bf
arvinpaundra <arvinpaundra06@gmail.com>
2022-11-06 00:47:33 +0700

    refactor: move payment gateway repository implementation

---
+++ internal/midtrans/midtrans_repository.go
@@ -0,0 +1,63 @@
+package pg_midtrans
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/arvinpaundra/go-rent-bike/configs"
+	"github.com/arvinpaundra/go-rent-bike/internal/dto"
+	"github.com/arvinpaundra/go-rent-bike/internal/repository"
+	"github.com/midtrans/midtrans-go"
+	"github.com/midtrans/midtrans-go/snap"
+)
+
+var snapClient snap.Client
+
+type PaymentGatewayRepository struct{}
+
+func (r PaymentGatewayRepository) InitializeClientMidtrans() {
+	snapClient.New(configs.Cfg.MidtransServerKeyDev, midtrans.Sandbox)
+}
+
+func (r PaymentGatewayRepository) CreateTransaction(req dto.PaymentGateway) string {
+	snapUrl, err := snapClient.CreateTransactionToken(generateSnapReq(req))
+
+	if err != nil {
+		fmt.Printf("Midtrans error : %v", err.GetMessage())
+	}
+
+	return snapUrl
+}
+
+func (r PaymentGatewayRepository) CreateUrlTransactionWithGateway(req dto.PaymentGateway) string {
+	snapClient.Options.SetContext(context.Background())
+
+	snapUrl, err := snapClient.CreateTransactionUrl(generateSnapReq(req))
+
+	if err != nil {
+		fmt.Printf("Midtrans error : %v", err.GetMessage())
+	}
+
+	return snapUrl
+}
+
+func generateSnapReq(req dto.PaymentGateway) *snap.Request {
+	reqSnap := &snap.Request{
+		TransactionDetails: midtrans.TransactionDetails{
+			OrderID:  req.OrderId,
+			GrossAmt: req.GrossAmt,
+		},
+		EnabledPayments: snap.AllSnapPaymentType,
+		CustomerDetail: &midtrans.CustomerDetails{
+			Email: req.Email,
+			Phone: req.Phone,
+		},
+		Items: &req.Items,
+	}
+
+	return reqSnap
+}
+
+func NewPaymentGatewayRepository() repository.PaymentGatewayRepository {
+	return PaymentGatewayRepository{}
+}
