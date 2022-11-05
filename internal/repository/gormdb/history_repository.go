0899599a870ff206b118ac10a6355d313da331ca
arvinpaundra <arvinpaundra06@gmail.com>
2022-11-05 15:07:22 +0700

    feat(user-histories): Add repository implementation for History

---
+++ internal/repository/gormdb/history_repository.go
@@ -0,0 +1,71 @@
+package gormdb
+
+import (
+	"errors"
+
+	"github.com/arvinpaundra/go-rent-bike/database"
+	"github.com/arvinpaundra/go-rent-bike/internal"
+	"github.com/arvinpaundra/go-rent-bike/internal/model"
+	"github.com/arvinpaundra/go-rent-bike/internal/repository"
+	"gorm.io/gorm"
+)
+
+type HistoryRepository struct {
+	DB *gorm.DB
+}
+
+func (r HistoryRepository) Create(historyUC model.History) error {
+	err := database.DB.Model(&model.History{}).Create(&historyUC).Error
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r HistoryRepository) FindAll(userId string) (*[]model.History, error) {
+	histories := &[]model.History{}
+
+	err := database.DB.Model(&model.History{}).Preload("Order", "user_id = ?", userId).Find(&histories).Error
+
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, internal.ErrRecordNotFound
+		}
+
+		return nil, err
+	}
+
+	return histories, nil
+}
+
+func (r HistoryRepository) FindByIdOrder(orderId string) (*model.History, error) {
+	history := &model.History{}
+
+	err := database.DB.Model(&model.History{}).Where("order_id = ?", orderId).Take(&history).Error
+
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, internal.ErrRecordNotFound
+		}
+
+		return nil, err
+	}
+
+	return history, nil
+}
+
+func (r HistoryRepository) Update(orderId string, historyUC model.History) error {
+	err := database.DB.Model(&model.History{}).Where("order_id = ?", orderId).Save(&historyUC).Error
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func NewHistoryRepository(db *gorm.DB) repository.HistoryRepository {
+	return HistoryRepository{db}
+}
