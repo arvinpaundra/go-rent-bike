8798bc096ea416ca7791cfddf4cf15d6c3b141aa
arvinpaundra <arvinpaundra06@gmail.com>
2022-11-05 22:28:10 +0700

    refactor: edit route naming

--- internal/route/route.go
+++ internal/route/route.go
@@ -1,10 +1,13 @@
 package route
 
 import (
+	"github.com/arvinpaundra/go-rent-bike/configs"
 	controller "github.com/arvinpaundra/go-rent-bike/internal/controller/rest-http"
+	mddlwrs "github.com/arvinpaundra/go-rent-bike/internal/middlewares"
 	"github.com/arvinpaundra/go-rent-bike/internal/repository/gormdb"
 	"github.com/arvinpaundra/go-rent-bike/internal/usecase"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"gorm.io/gorm"
 )
 
@@ -27,10 +30,12 @@ func New(db *gorm.DB, e *echo.Echo) {
 	auth.POST("/register", userController.HandlerRegister)
 	auth.POST("/login", userController.HandlerLogin)
 
-	u := v1.Group("/customers")
+	u := v1.Group("/customers", middleware.JWT([]byte(configs.Cfg.JWTSecret)))
 	u.GET("", userController.HandlerFindAllUsers)
-	u.GET("/:id", userController.HandlerFindUserById)
 	u.GET("/:id/histories", userController.HandlerFindAllUserHistories)
+	u.GET("/:id/orders", userController.HandlerFindAllOrdersUser)
+	u.GET("/:id/orders/:orderId", userController.HandlerFindByIdOrderUser)
+	u.GET("/:id", userController.HandlerFindUserById)
 	u.PUT("/:id", userController.HandlerUpdateUser)
 	u.DELETE("/:id", userController.HandlerDeleteUser)
 
@@ -40,11 +45,11 @@ func New(db *gorm.DB, e *echo.Echo) {
 	renterController := controller.NewRenterController(renterUsecase)
 
 	r := v1.Group("/renters")
-	r.POST("", renterController.HandlerCreateRenter)
+	r.POST("", renterController.HandlerCreateRenter, middleware.JWT([]byte(configs.Cfg.JWTSecret)))
 	r.GET("", renterController.HandlerFindAllRenters)
 	r.GET("/:id", renterController.HandlerFindRenterById)
-	r.PUT("/:id", renterController.HandlerUpdateRenter)
-	r.DELETE("/:id", renterController.HandlerDeleteRenter)
+	r.PUT("/:id", renterController.HandlerUpdateRenter, middleware.JWT([]byte(configs.Cfg.JWTSecret)), mddlwrs.CheckIsRenter)
+	r.DELETE("/:id", renterController.HandlerDeleteRenter, middleware.JWT([]byte(configs.Cfg.JWTSecret)), mddlwrs.CheckIsRenter)
 
 	// category
 	categoryRepository := gormdb.NewCategoryRepositoryGorm(db)
@@ -52,11 +57,11 @@ func New(db *gorm.DB, e *echo.Echo) {
 	categoryController := controller.NewCategoryController(categoryUsecase)
 
 	c := v1.Group("/categories")
-	c.POST("", categoryController.HandlerCreateCategory)
+	c.POST("", categoryController.HandlerCreateCategory, middleware.JWT([]byte(configs.Cfg.JWTSecret)), mddlwrs.CheckIsRenter)
 	c.GET("", categoryController.HandlerFindAllCategories)
 	c.GET("/:id", categoryController.HandlerFindCategoryById)
-	c.PUT("/:id", categoryController.HandlerUpdateCategory)
-	c.DELETE("/:id", categoryController.HandlerDeleteCategory)
+	c.PUT("/:id", categoryController.HandlerUpdateCategory, middleware.JWT([]byte(configs.Cfg.JWTSecret)), mddlwrs.CheckIsRenter)
+	c.DELETE("/:id", categoryController.HandlerDeleteCategory, middleware.JWT([]byte(configs.Cfg.JWTSecret)), mddlwrs.CheckIsRenter)
 
 	// bike
 	bikeRepository := gormdb.NewBikeRepositoryGorm(db)
@@ -64,23 +69,21 @@ func New(db *gorm.DB, e *echo.Echo) {
 	bikeController := controller.NewBikeController(bikeUsecase)
 
 	b := v1.Group("/bikes")
-	b.POST("", bikeController.HandlerAddNewBike)
+	b.POST("", bikeController.HandlerAddNewBike, middleware.JWT([]byte(configs.Cfg.JWTSecret)), mddlwrs.CheckIsRenter)
 	b.GET("", bikeController.HandlerFindAllBikes)
 	b.GET("/renters/:renterId", bikeController.HandlerFindBikesByRenter)
 	b.GET("/categories/:categoryId", bikeController.HandlerFindBikesByCategory)
 	b.GET("/:id", bikeController.HandlerFindByIdBike)
-	b.PUT("/:id", bikeController.HandlerUpdateBike)
-	b.DELETE("/:id", bikeController.HandlerDeleteBike)
-	b.POST("/:id/reviews", bikeController.HandlerCreateNewBikeReview)
+	b.PUT("/:id", bikeController.HandlerUpdateBike, middleware.JWT([]byte(configs.Cfg.JWTSecret)), mddlwrs.CheckIsRenter)
+	b.DELETE("/:id", bikeController.HandlerDeleteBike, middleware.JWT([]byte(configs.Cfg.JWTSecret)), mddlwrs.CheckIsRenter)
+	b.POST("/:id/reviews", bikeController.HandlerCreateNewBikeReview, middleware.JWT([]byte(configs.Cfg.JWTSecret)))
 
 	// order
 	orderRepository := gormdb.NewOrderRepository(db)
 	orderUsecase := usecase.NeworderUsecase(orderRepository)
 	orderController := controller.NewOrderController(orderUsecase)
 
-	o := v1.Group("/orders")
+	o := v1.Group("/orders", middleware.JWT([]byte(configs.Cfg.JWTSecret)))
 	o.POST("", orderController.HandlerCreateNewOrder)
-	o.GET("/customers/:userId", orderController.HandlerFindAllOrdersUser)
-	o.GET("/:orderId/customers", orderController.HandlerFindByIdOrderUser)
-	o.GET("/:orderId/return", orderController.HandlerReturnBike)
+	o.GET("/:id/return", orderController.HandlerReturnBike)
 }
