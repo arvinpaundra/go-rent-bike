1450443aff48861565760a70238bcb65eb06a5b9
arvinpaundra <arvinpaundra06@gmail.com>
2022-11-07 17:23:04 +0700

    refactor: add Payment field

--- internal/model/payment.go
+++ internal/model/payment.go
@@ -6,6 +6,7 @@ type Payment struct {
 	ID            string    `json:"id" gorm:"size:255"`
 	PaymentStatus string    `json:"payment_status" gorm:"size:20"`
 	PaymentType   string    `json:"payment_type" gorm:"size:50"`
+	PaymentLink   string    `json:"payment_link" gorm:"size:255"`
 	CreatedAt     time.Time `json:"created_at"`
 	UpdatedAt     time.Time `json:"updated_at"`
 }
